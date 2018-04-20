c23301cb29d046e2cfac48633558f35352554423
Arshin Jain <arshinx@users.noreply.github.com>
2018-04-20 17:42:41 -0400

    Mongo Connection

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var bodyParser  = require('body-parser');
 var mongoose    = require('mongoose');
 var app = express();
 
+// Mongo DB Connection
+mongoose.connect("mongodb://localhost:27017/express-auth");
+
 // parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
