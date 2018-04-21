03c5704c7595685a6b62a6b83ed051a0092c6e7a
Arshin Jain <arshinx@users.noreply.github.com>
2018-04-20 23:28:04 -0400

    capitalize

--- models/user.js
+++ models/user.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 
-var userSchema = new mongoose.Schema({
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     unique: true,
     required: true,
     trim: true // removes whitespace
+  },
   name: {
     type: String,
     required: true,
