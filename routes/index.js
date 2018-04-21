0687c3dcebdc1845f3076d79e67548c0aaeee5ab
Arshin Jain <arshinx@users.noreply.github.com>
2018-04-21 15:15:12 -0400

    Bug fixes

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.post('/login', function(req, res, next) {
   if (req.body.email && req.body.password) {
     User.authenticate(req.body.email, req.body.password, function (error, user) {
       if (error || !user) {
-        var err == new Error('Wrong email or password.');
+        var err = new Error('Wrong email or password.');
         err.status = 401;
         return next(err);
       } else {
