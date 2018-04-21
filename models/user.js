5f10ac4301abba1d1aee1f56cf9d9ba5d30686fa
Arshin Jain <arshinx@users.noreply.github.com>
2018-04-20 23:27:02 -0400

    User Schema

--- models/user.js
+++ models/user.js
@@ -1,1 +1,10 @@
 var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true // removes whitespace
+  }
+})
