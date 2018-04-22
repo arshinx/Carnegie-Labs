42dd642cf88e432542e3a66d3786304730b2d5c9
Arshin Jain <arshinx@users.noreply.github.com>
2018-04-21 23:21:02 -0400

    import module

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var mongoose    = require('mongoose');
 var session     = require('express-session');
+var MongoStore  = require('connect-mongo')(session);
 var app = express();
 
 // Use sessions for tracking logins
