74fb78e257cb7b81c567223d291b56fd5e40bfdb
Arshin Jain <arshinx@users.noreply.github.com>
2018-04-21 23:06:53 -0400

    Export Middleware

--- middleware/index.js
+++ middleware/index.js
@@ -4,4 +4,8 @@ function loggedOut(req, res, next) {
   if (req.session && req.session.userId) {
     return res.redirect('/profile');
   }
+  return next();
 }
+
+// Export Middleware
+module.exports.loggedOut = loggedOut;
