a1af5255369f624d76b55be15e9ea176556352cb
Arshin Jain <arshinx@users.noreply.github.com>
2018-04-21 23:09:11 -0400

    login route - middleware

--- routes/index.js
+++ routes/index.js
@@ -58,7 +58,7 @@ router.post('/login', function(req, res, next) {
 });
 
 // GET /login
-router.get('/login', function(req, res, next) {
+router.get('/login', mid.loggedOut, function(req, res, next) {
   return res.render('login', { title: 'Log In'});
 });
 
