842089dd6b9abcb9ff9c1d66d9c989f0dbc97bc8
Arshin Jain <arshinx@users.noreply.github.com>
2018-04-21 23:16:28 -0400

    reorganize

--- middleware/index.js
+++ middleware/index.js
@@ -12,13 +12,14 @@ function loggedOut(req, res, next) {
 function requiresLogin(req, res, next) {
   // logged in?
   if (req.session && req.session.userId) {
-
+    // Continue without doing something
+    return next();
   } else {
+    // Error
     var err = new Error('Must be logged in to view this page.');
     err.status = 401;
     return next(err);
   }
-  return next();
 }
 
 // Export Middleware
