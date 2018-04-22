bb5d1994e902c2cc977ca4f1579f6817f6ed5a72
Arshin Jain <arshinx@users.noreply.github.com>
2018-04-21 23:11:29 -0400

    Requires Login Middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,16 @@
 
+// Logged Out Middleware
 function loggedOut(req, res, next) {
   // logged in?
   if (req.session && req.session.userId) {
     return res.redirect('/profile');
   }
   return next();
+}
+
+// Requires Login Middleware
+function requiresLogin(req, res, next) {
+
 }
 
 // Export Middleware
